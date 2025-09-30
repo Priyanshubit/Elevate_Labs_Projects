2b5adbcf46d68f6de75636fdeac7fd7da1ee2a8c
Priyanshu Bagri <98304073+Priyanshubit@users.noreply.github.com>
2025-09-30 22:54:26 +0530

    Delete TASK 6: Sales Trend Analysis Using Aggregations directory

--- TASK 6: Sales Trend Analysis Using Aggregations/Sheet2.sql
+++
@@ -1,5 +0,0 @@
--- Last 12 months only--
-
-select sum(order_value)
-from synthetic_ecommerce_orders
-WHERE order_date >= DATE_SUB(CURDATE(), INTERVAL 12 MONTH);
--- TASK 6: Sales Trend Analysis Using Aggregations/Sheet3.sql
+++
@@ -1,5 +0,0 @@
--- Specific for 2023 --
-
-select sum(order_value)
-from synthetic_ecommerce_orders
-WHERE EXTRACT(YEAR FROM order_date) = 2023;
